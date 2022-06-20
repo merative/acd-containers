183f9d535dd7d58b1be1cfe406cb77e5b15c8ffa
dcwebermn <43646284+dcwebermn@users.noreply.github.com>
2022-06-20 10:36:44 -0500

    add crtg deploy scripts and tests

---
+++ ibm-wh-acd-ce/ibm-wh-acd-ce/crtg/tests/run-fvttests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Licensed Materials - Property of IBM
+#
+# Copyright IBM Corp. 2012, 2019 All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+
+#########################################################################
+#Global vars
+#########################################################################
+SCRIPT_NAME="$(basename "$0")"
+echo "####################"
+echo "Executing $SCRIPT_NAME"
+
+./tests/run-test.sh "fvttest" "MVT"
+exit $?
