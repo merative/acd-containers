74df64cc6d23128f5f68dc33d24544379fd43bb7
Brian <bcragun@merative.com>
2022-11-23 12:39:03 -0700

    Fix to find right favicon
    Signed-off-by: Brian <bcragun@merative.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         background_color: '#ffffff',
         theme_color: '#0062ff',
         display: 'browser',
-        icon: './src/gatsby-theme-carbon/images/favicon.svg'
+        icon: 'src/images/favicon.svg'
       },
     },
     {
@@ -30,6 +30,7 @@ module.exports = {
         isSearchEnabled: true,
         withWebp: true,
         imageQuality: 75,
+        iconPath: 'src/images/favicon.svg'
       },
     },
   ],
